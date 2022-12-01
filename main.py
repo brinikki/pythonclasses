c975957cec0bb7cb8f1dc45aa3bb69bda76040ef
ieee754 <94340126+brinikki@users.noreply.github.com>
2022-12-01 21:48:37 +0000

    final lab rip

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-class Person:
-  def __init__(self, name, age):
+class Pet:
+  def __init__ (self, name, age, type):
     self.name = name
     self.age = age
-my_object = Person("Evan", 22)
+    self.type = type
+    
+  def myfunc(self):
+      #print("Hello my name is " + self.name)
+    if self.type == "cat":
+      print("meOW")
+    elif self.type == "dog":
+      print("WOOF")
+    
+    
+my_object = Pet("Evan", 22, "cat")
+my_object2 = Pet("Charles", 2, "dog")
 #The self parameter is a reference to the current instance of the class
-print (my_object.name)
-print (my_object.age)
-print (my_object.name, 'is', my_object.age, 'years old')
-
-
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-def myfunc(self):
-  print("Hello my name is " + self.name)
-my_object = Person("John", 36)
-my_object.age = 40
+#print (my_object.name)
+#print (my_object.age)
+#print (my_object.name, 'is', my_object.age, 'years old')
 my_object.myfunc()
-print(my_object.age)
-#You can delete an object with:
-del my_object
+my_object2.myfunc()
